a298959a9be36c514155371e1ed5420c9db2301a
sivakrishnavegi <sivakrishnavegi@yahoo.com>
2025-08-07 08:50:27 +0530

    feat : #1 added primsa

---
+++ src/app/(auth)/callback/page.tsx
@@ -0,0 +1,8 @@
+export const dynamic = 'force-dynamic';
+const AuthCallbackPage = () => {
+return <div>Auth callback</div>
+}
+
+export default AuthCallbackPage;
+
+//TODO: Implement the actual callback logic
---
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,9 @@
+import { SignUp } from '@clerk/nextjs'
+
+const SignUPPage = () => {
+  return (
+    <SignUp />
+  )
+}
+
+export default SignUPPage
